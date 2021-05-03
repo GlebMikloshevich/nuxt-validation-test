fec10b3ce4225de6925f29c63f0346ea9ba66de5
GlebMikloshevich <glebmikloshevich@gmail.com>
2021-05-03 19:21:10 +0800

    Initial commit. 3 basic forms

---
+++ layouts/default.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <nav
+      class="navbar header has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          href="/"
+        >
+          <img
+            src="~assets/buefy.png"
+            alt="Buefy"
+            height="28"
+          >
+        </a>
+
+        <div class="navbar-burger">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+    </nav>
+
+    <section class="main-content columns">
+      <aside class="column is-2 section">
+        <p class="menu-label is-hidden-touch">
+          General
+        </p>
+        <ul class="menu-list">
+          <li
+            v-for="(item, key) of items"
+            :key="key"
+          >
+            <nuxt-link
+              :to="item.to"
+              exact-active-class="is-active"
+            >{{ item.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </aside>
+
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'auto_validation',
+          to: { name: 'auto_validation' },
+        },
+        {
+          title: 'delayed auto validation',
+          to: { name: 'delayed_auto_validation' },
+        },
+        {
+          title: 'on click validation',
+          to: { name: 'on_click_validation' },
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ pages/on_click_validation.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <section>
+      <h1>On click validation</h1>
+      <b-field label="Фамилия*">
+        <b-input v-model="lastname"></b-input>
+      </b-field>
+      <b-field label="Имя*">
+        <b-input v-model="name"></b-input>
+      </b-field>
+      <b-field label="Отчество">
+        <b-input v-model="middlename"></b-input>
+      </b-field>
+      <b-field label="Email*">
+        <b-input type="email"></b-input>
+      </b-field>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'on_click_validation',
+};
+</script>
+
+<style scoped>
+
+</style>
